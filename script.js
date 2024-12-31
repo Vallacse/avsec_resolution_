5495c000d394db169b0fd0ece8bdd9f9b3d5fe88
vallarasu <vallarasup245@example.com>
2024-12-31 09:20:52 +0530

    g

--- script.js
+++ script.js
@@ -210,7 +210,7 @@ h1 {
 }
 
 
-@media (max-width: 768px) {
+@media (max-width: 900px) {
 
 body {
     font-family: 'Arial', sans-serif;
@@ -222,8 +222,6 @@ body {
     width: 100%;
 }
     .certificate {
-        margin-left: 50px;
-        padding-left: 50px;
         width: 600px;
         height: 800px;
     }
